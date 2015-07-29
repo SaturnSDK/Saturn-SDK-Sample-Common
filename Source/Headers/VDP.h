f3a854a91fd78c23a9d87c725875aebb7ddc1112
Rico Tyrell <teh_programerer@interactivelaboratories.com>
2015-07-29 23:17:59 +0100

    Added sprite rendering functions for VDP1

--- Source/Headers/VDP.h
+++ Source/Headers/VDP.h
@@ -6,7 +6,7 @@
 /* VDP1 */
 /* Base address of VDP1 */
 #define VDP1_BASE	0x25C00000
-#define VDP1_VRAM	( *( volatile uint16_t * )( VDP1_BASE ) )
+#define VDP1_VRAM	VDP1_BASE
 /* TV mode selection */
 #define VDP1_TVMR	( *( volatile uint16_t * )( VDP1_BASE + 0x100000 ) )
 /* Frame buffer switch mode */
@@ -30,6 +30,33 @@
 /* Mode status */
 #define VDP1_MODR	( *( volatile uint16_t * )( VDP1_BASE + 0x100016 ) )
 
+/* VDP1 command offets */
+/* Control */
+#define VDP1_CMD_CTRL	0x00
+/* Link specification */
+#define VDP1_CMD_LINK	0x01
+/* Draw mode */
+#define VDP1_CMD_PMOD	0x02
+/* Color control */
+#define VDP1_CMD_COLR	0x03
+/* Character address */
+#define VDP1_CMD_SRCA	0x04
+/* Character size */
+#define VDP1_CMD_SIZE	0x05
+/* Vertex coordinate data XA-YD */
+#define VDP1_CMD_XA		0x06
+#define VDP1_CMD_YA		0x07
+#define VDP1_CMD_XB		0x08
+#define VDP1_CMD_YB		0x09
+#define VDP1_CMD_XC		0x0A
+#define VDP1_CMD_YC		0x0B
+#define VDP1_CMD_XD		0x0C
+#define VDP1_CMD_YD		0x0D
+/* Gouraud shading table */
+#define VDP1_CMD_GRDA	0x0E
+/* Unused, dummy */
+#define VDP1_CMD_DUMY	0x0F
+
 /* VDP2 */
 #define VDP2_BASE	0x25E00000
 #define VDP2_VRAM	VDP2_BASE
@@ -38,13 +65,46 @@
 #define VDP2_TVMD	( *( volatile uint16_t * )( 0x25F80000 ) )
 #define VDP2_TVSTAT	( *( volatile uint16_t * )( 0x25F80004 ) )
 
+/* Sprite priority */
+#define VDP2_PRISA	( *( volatile uint16_t * )( VDP2_REG + 0xF0 ) )
+#define VDP2_PRISB	( *( volatile uint16_t * )( VDP2_REG + 0xF2 ) )
+#define VDP2_PRISC	( *( volatile uint16_t * )( VDP2_REG + 0xF4 ) )
+#define VDP2_PRISD	( *( volatile uint16_t * )( VDP2_REG + 0xF6 ) )
+
+/* Background priority */
+#define VDP2_PRINA	( *( volatile uint16_t * )( VDP2_REG + 0xF8 ) )
+#define VDP2_PRIMB	( *( volatile uint16_t * )( VDP2_REG + 0xFA ) )
+
+/* Pointer access to VDP1 addresses */
+extern volatile uint16_t *VDP1_VRAM_PTR;
+
 /* Pointer access to VDP2 addresses */
 extern volatile uint16_t *VDP2_VRAM_PTR;
 extern volatile uint16_t *VDP2_CRAM_PTR;
 extern volatile uint16_t *VDP2_REG_PTR;
 
+/* Initialise and shutdown */
 void VDP_Initialize( void );
 void VDP_Shutdown( void );
+
+/* VDP1 functions */
+/* Reset the command list */
+void VDP1_ClearCommandList( void );
+/* Signal the end of the command list */
+void VDP1_EndCommandList( void );
+/* Set the width and height of the clip coordinates */
+void VDP1_SetSystemClipCoordinates( int16_t p_X, int16_t p_Y );
+/* Set the upper-left and lower-right of the user clip coordinates */
+void VDP1_SetUserClipCoordinates( int16_t p_X0, int16_t p_Y0,
+	int16_t p_X1, int16_t p_Y1 );
+/* Set up the local draw coordinates */
+void VDP1_SetLocalCoordinates( int16_t p_X, int16_t p_Y );
+/* Sprite drawing */
+/* - Normal sprite */
+void VDP1_DrawSpriteNormalRGB( int16_t p_X, int16_t p_Y,
+	int16_t p_Width, int8_t p_Height, uint32_t p_Texture );
+
+/* VDP2 functions */
 void VDP2_SetBackgroundColor( uint8_t p_Red, uint8_t p_Green, uint8_t p_Blue );
 
 /* Polling V-Blank and H-Blank */
@@ -54,6 +114,10 @@ void VDP_WaitVBlankOut( void );
 void VDP_WaitVBlankIn( void );
 void VDP_WaitHBlank( void );
 void VDP_WaitVBlank( void );
+
+/* Global variables */
+/* Keep track of the command list offset */
+extern uint16_t	VDP1_CommandListIndex;
 
 #endif /* __SSSDK_COMMON_VDP_H__ */
 
--- Source/Source/VDP.c
+++ Source/Source/VDP.c
@@ -1,8 +1,13 @@
 #include <VDP.h>
+#include <USBCart.h>
+
+volatile uint16_t *VDP1_VRAM_PTR	= ( volatile uint16_t * )( VDP1_VRAM );
 
 volatile uint16_t *VDP2_VRAM_PTR	= ( volatile uint16_t * )( VDP2_VRAM );
 volatile uint16_t *VDP2_CRAM_PTR	= ( volatile uint16_t * )( VDP2_CRAM );
 volatile uint16_t *VDP2_REG_PTR		= ( volatile uint16_t * )( VDP2_REG );
+
+uint16_t VDP1_CommandListIndex = 0;
 
 
 void VDP_Initialize( void )
@@ -65,6 +70,96 @@ void VDP_Shutdown( void )
 	{
 		VDP2_CRAM_PTR[ Index ] = 0x0000;
 	}
+}
+
+void VDP1_ClearCommandList( void )
+{
+	/* Just reset the command list offset */
+	VDP1_CommandListIndex = 0;
+}
+
+void VDP1_EndCommandList( void )
+{
+	uint16_t *pListPtr =
+		( uint16_t * )( VDP1_VRAM + ( VDP1_CommandListIndex << 5 ) );
+
+	pListPtr[ VDP1_CMD_CTRL ] = 0x8000;
+	++VDP1_CommandListIndex;
+}
+
+void VDP1_SetSystemClipCoordinates( int16_t p_X, int16_t p_Y )
+{
+	uint16_t *pListPtr =
+		( uint16_t * )( VDP1_VRAM + ( VDP1_CommandListIndex << 5 ) );
+	
+	/* System clip coordinates */
+	pListPtr[ VDP1_CMD_CTRL ] = 0x0009;
+	/* Continue to the next command */
+	pListPtr[ VDP1_CMD_LINK ] = 0x0000;
+	/* Set the coordinates */
+	pListPtr[ VDP1_CMD_XC ] = p_X;
+	pListPtr[ VDP1_CMD_YC ] = p_Y;
+
+	++VDP1_CommandListIndex;
+}
+
+void VDP1_SetUserClipCoordinates( int16_t p_X0, int16_t p_Y0,
+	int16_t p_X1, int16_t p_Y1 )
+{
+	uint16_t *pListPtr =
+		( uint16_t * )( VDP1_VRAM + ( VDP1_CommandListIndex << 5 ) );
+	
+	/* User clip coordinates */
+	pListPtr[ VDP1_CMD_CTRL ] = 0x0008;
+
+	pListPtr[ VDP1_CMD_LINK ] = 0x0000;
+	pListPtr[ VDP1_CMD_XA ] = p_X0;
+	pListPtr[ VDP1_CMD_YA ] = p_Y0;
+	pListPtr[ VDP1_CMD_XC ] = p_X1;
+	pListPtr[ VDP1_CMD_YC ] = p_Y1;
+
+	++VDP1_CommandListIndex;
+}
+
+void VDP1_SetLocalCoordinates( int16_t p_X, int16_t p_Y )
+{
+	uint16_t *pListPtr =
+		( uint16_t * )( VDP1_VRAM + ( VDP1_CommandListIndex << 5 ) );
+	
+	/* Local coordinates */
+	pListPtr[ VDP1_CMD_CTRL ] = 0x000A;
+	/* Continue to the next command */
+	pListPtr[ VDP1_CMD_LINK ] = 0x0000;
+	/* Set the local coordinates */
+	pListPtr[ VDP1_CMD_XA ] = p_X;
+	pListPtr[ VDP1_CMD_YA ] = p_Y;
+
+	++VDP1_CommandListIndex;
+}
+
+void VDP1_DrawSpriteNormalRGB( int16_t p_X, int16_t p_Y,
+	int16_t p_Width, int8_t p_Height, uint32_t p_Texture )
+{
+	uint16_t *pListPtr =
+		( uint16_t * )( VDP1_VRAM + ( VDP1_CommandListIndex << 5 ) );
+
+	/* Normal sprite */
+	pListPtr[ VDP1_CMD_CTRL ] = 0x0000;
+	/* Continue to the next command */
+	pListPtr[ VDP1_CMD_LINK ] = 0x0000;
+	/* Set color mode to RGB */
+	pListPtr[ VDP1_CMD_PMOD ] = 0x00A8;
+	/* Set the texture pointer */
+	pListPtr[ VDP1_CMD_SRCA ] = p_Texture >> 3;
+	/* The Width is divided by 8 before being set, height is allowed as-is
+	 * Bits 15 and 14 are zero */
+	pListPtr[ VDP1_CMD_SIZE ] =
+		( ( ( p_Width >> 3 ) << 8 ) | ( p_Height & 0xFF ) ) & 0x3FFF;
+	/* Set the position */
+	pListPtr[ VDP1_CMD_XA ] = p_X;
+	pListPtr[ VDP1_CMD_YA ] = p_Y;
+
+	++VDP1_CommandListIndex;
 }
 
 void VDP2_SetBackgroundColor( uint8_t p_Red, uint8_t p_Green, uint8_t p_Blue )
